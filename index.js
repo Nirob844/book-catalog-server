5e174a230adad2b7c7622b3ecb90c21717624361
Nirob Hasan <nirob427417@gmail.com>
2023-07-20 16:43:45 +0600

    authorization

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 
 const cors = require('cors');
 
@@ -10,7 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9mv6kq4.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,8 +27,6 @@ const run = async () => {
     
 
     app.get('/books', async (req, res) => {
-    //  const cursor = bookCollection.find({});
-    //  const books = await cursor.toArray();
      const { search, genre, publicationYear } = req.query;
      // Prepare the filter conditions
      const filter = {};
@@ -53,30 +52,142 @@ const run = async () => {
        };
      }
      const books = await bookCollection.find(filter).toArray();
-      res.send({ status: true, data: books });
+     return res.status(200).send({
+      message: "Books retrieved successfully!",
+      data: books,
+    });
+    });
+
+    app.get("/books/recent-published", async (req, res) => {
+      const sort = { publishedDate: -1 };
+      const result = await booksCollection
+        .find({})
+        .sort(sort)
+        .limit(10)
+        .toArray();
+      return res.status(200).send({
+        message: "Recent Published Books retrieved successfully!",
+        books: result,
+      });
     });
 
     app.post('/book', async (req, res) => {
-      const book = req.body;
-
-      const result = await bookCollection.insertOne(book);
-
-      res.send(result);
+      const authorizeToken = req.headers.authorization;
+      if (!authorizeToken) {
+        return res.status(400).send({
+          message: "Authorization not provided",
+        });
+      } else {
+        const verifiedUser = await jwt.verify(authorizeToken, "tokenSecret");
+        if (!verifiedUser) {
+          return res.status(400).send({
+            message: "You are not authorized",
+          });
+        } else {
+          const book = req.body;
+          const result = await bookCollection.insertOne(book);
+          if (result.acknowledged == true) {
+            return res.status(200).send({
+              message: "Book added successfully!",
+              book: book,
+            });
+          } else {
+            return res.status(400).send({
+              message: "Book added failed!",
+            });
+          }
+        }
+      }
     });
 
     app.get('/book/:id', async (req, res) => {
       const id = req.params.id;
 
-      const result = await bookCollection.findOne({ _id: ObjectId(id) });
-      res.send(result);
+      const book = await bookCollection.findOne({ _id: ObjectId(id) });
+      //res.send(result);
+      if (book) {
+        return res.status(200).send({
+          message: "Book details retrieved successfully!",
+          book: book,
+        });
+      } else {
+        return res.status(404).send({
+          message: "Book not found",
+        });
+      }
+    });
+
+ app.put("/book/:id", async (req, res) => {
+      const authorizeToken = req.headers.authorization;
+      if (!authorizeToken) {
+        return res.status(400).send({
+          message: "Authorization not provided",
+        });
+      } else {
+        const verifiedUser = jwt.verify(authorizeToken, "tokenSecret");
+        if (!verifiedUser) {
+          return res.status(400).send({
+            message: "You are not authorized",
+          });
+        } else {
+          const bookId = req.params.id;
+          const updatedBookData = req.body;
+
+          // Remove the _id field from the updatedBookData object
+          delete updatedBookData._id;
+
+          const result = await bookCollection.updateOne(
+            { _id: new ObjectId(bookId) },
+            { $set: updatedBookData }
+          );
+
+          if (result.matchedCount > 0) {
+            return res.status(200).send({
+              message: "Book updated successfully!",
+              book: updatedBookData,
+            });
+          } else {
+            return res.status(404).send({
+              message: "Book not found",
+            });
+          }
+        }
+      }
     });
 
     app.delete('/book/:id', async (req, res) => {
-      const id = req.params.id;
+      const authorizeToken = req.headers.authorization;
+      if (!authorizeToken) {
+        return res.status(400).send({
+          message: "Authorization not provided",
+        });
+      } else {
+        const verifiedUser = await jwt.verify(authorizeToken, "tokenSecret");
+        if (!verifiedUser) {
+          return res.status(400).send({
+            message: "You are not authorized",
+          });
+        } else {
+          const bookId = req.params.id;
 
-      const result = await bookCollection.deleteOne({ _id: ObjectId(id) });
-      res.send(result);
+          const result = await bookCollection.deleteOne({
+            _id: new ObjectId(bookId),
+          });
+
+          if (result.deletedCount > 0) {
+            return res.status(200).send({
+              message: "Book deleted successfully!",
+            });
+          } else {
+            return res.status(404).send({
+              message: "Book not found",
+            });
+          }
+        }
+      }
     });
+
+   
 
     app.post('/comment/:id', async (req, res) => {
       const bookId = req.params.id;
@@ -87,8 +198,6 @@ const run = async () => {
         { _id: ObjectId(bookId) },
         { $push: { comments: comment } }
       );
-
-      console.log(result);
 
       if (result.modifiedCount !== 1) {
         console.error('book not found or comment not added');
@@ -114,13 +223,65 @@ const run = async () => {
       }
     });
 
-    app.post('/user', async (req, res) => {
-      const user = req.body;
-
-      const result = await userCollection.insertOne(user);
-
-      res.send(result);
+   // Authentication APIs Start
+    app.post("/auth/signup", async (req, res) => {
+      const userData = req.body;
+      // find user is exist or not
+      const isExistUser = await usersCollection.findOne({
+        email: userData.email,
+      });
+      if (isExistUser) {
+        return res.status(400).send({
+          message: "This email already exist!",
+        });
+      } else {
+         const hashedPassword = await bcrypt.hash(userData.password, 12);
+        userData.password = hashedPassword;
+        const result = await usersCollection.insertOne(userData);
+        if (result.acknowledged == true) {
+          return res.status(200).send({
+            message: "User sign up successfully!",
+          });
+        } else {
+          return res.status(400).send({
+            message: "Sign Up Failed!",
+          });
+        }
+      }
     });
+
+    app.post("/auth/login", async (req, res) => {
+      const userData = req.body;
+      const isAvailableUser = await usersCollection.findOne({
+        email: userData.email,
+      });
+      if (!isAvailableUser) {
+        return res.status(400).send({
+          message: "This email does not exist!",
+        });
+      } else {
+        const isPasswordMatched = await bcrypt.compare(
+          userData.password,
+          isAvailableUser.password
+        );
+        if (!isPasswordMatched) {
+          return res.status(400).send({
+            message: "Incorrect Password!",
+          });
+        } else {
+          const accessToken = await jwt.sign(
+            { email: isAvailableUser.email },
+            "tokenSecret",
+            { expiresIn: "30d" }
+          );
+          return res.status(200).send({
+            message: "Login successfully!",
+            token: accessToken,
+          });
+        }
+      }
+    });
+
 
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email;
